9d2f66761f979ee6433ea2fe3a27077e34dd92b6
cbts96 <lexuanthang123@gmail.com>
2020-01-08 22:08:24 +0700

    Updates

---
+++ precache-manifest.7ca751714921e0fb08d096b623f4871f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eb6c8c1d649080d4210cc63c70e2ed4",
+    "url": "/Pokemon-rolodex/index.html"
+  },
+  {
+    "revision": "b16d1c5248dce565a384",
+    "url": "/Pokemon-rolodex/static/css/main.0aa3d863.chunk.css"
+  },
+  {
+    "revision": "c99fb85c0e6b9e5fbfc4",
+    "url": "/Pokemon-rolodex/static/js/2.6a66dadf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Pokemon-rolodex/static/js/2.6a66dadf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b16d1c5248dce565a384",
+    "url": "/Pokemon-rolodex/static/js/main.31fab061.chunk.js"
+  },
+  {
+    "revision": "a01f0255e3aca6ea6283",
+    "url": "/Pokemon-rolodex/static/js/runtime-main.2f1d7f9b.js"
+  },
+  {
+    "revision": "738aa749d1f333489dd20b54a3b15ee1",
+    "url": "/Pokemon-rolodex/static/media/pokemon_go_team_valor_team_red_4k.738aa749.jpg"
+  }
+]);
